e56be6a2c897e0dd2c2c3e2351d99064ab0d742d
Jahermon <jaher.front@gmail.com>
2023-05-02 00:05:24 +0100

    experimental

--- backend/app.js
+++ backend/app.js
@@ -10,9 +10,14 @@ const errorMiddlerware = require('./middlewares/error')
 
 // Setting up config file
 
+const corsOptions = {
+  origin: true,
+  credentials: true
+}
+
 dotenv.config({ path: 'backend/config/config.env' })
 
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({limit: '50mb',extended: true})); 
 app.use(cookieParser())
